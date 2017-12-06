f407b7d8a105fb6bea4850ae58afc70768f2f825
Sikandar Ali <sikandarali94@hotmail.com>
2017-12-06 20:34:55 +1100

    finished sec2 lec20 working with component styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /* In templateUrl and template we can decide if we want to link to an external template
+  or use an inline template. This is the same for styleUrls: we can link to an external
+  stylesheet using styleUrls. Please note that styleUrls has the stylesheet strings in an
+  array; this is because we can link multiple stylesheets to the component. If we want to
+  use an inline style we use the attribute named styles. We used back ticks (`) to allow
+  us to write the string in multiple lines.
+  */
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+        color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
 }
