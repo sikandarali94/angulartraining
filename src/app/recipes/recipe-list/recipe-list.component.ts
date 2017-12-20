be0f76f1d8fb19118f17eb44db2ce78dfdfc8d21
Sikandar Ali <sikandarali94@hotmail.com>
2017-12-20 16:34:38 +1100

    finished sec3 lec47 adding content to the recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+/* Here we first have to import the model that we want to
+use in the recipes list model.
+ */
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  /* Here we tell Typescript that recipes is the model we
+  defined. The square brackets next to Recipe tells Typescipt
+  that recipes is an array of Recipe models.
+   */
+  recipes: Recipe[] = [
+      /* Within the brackets we pass the arguments that the constructor
+      function requires as we defined the constructor in the Recipe model.
+       */
+      new Recipe('A Test Recipe', 'This is simply a test', 'http://dl.maxpixel.freegreatpicture.com/?f=food-1459693_1280.jpg&type=Download&token=717b704582bc9d84a2e69a9902ef91e2&pid=1459693')
+  ];
   constructor() { }
 
   ngOnInit() {
