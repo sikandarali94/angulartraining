6bcfb19bdc928a5ec637bdf1abb498fcc8f0212f
Sikandar Ali <sikandarali94@hotmail.com>
2017-12-21 22:52:52 +1100

    finished sec4 lec55 understanding angular error messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers;
+  /* Below we had changed the line from 'servers;' so the variable servers
+  can be defined. This change gets rid of the error:
+  Cannot read property 'push' of undefined.
+   */
+  servers = [];
 
   onAddServer() {
+    /* The line of code below is triggering the error:
+    Cannot read property 'push' of undefined. Because it is says 'of undefined'
+    it means this.servers is triggering the error. Of course if we look above
+    we indeed see that servers is undefined because we declared is as simply
+    'servers;' and haven't defined it.
+     */
     this.servers.push('Another Server');
   }
 
