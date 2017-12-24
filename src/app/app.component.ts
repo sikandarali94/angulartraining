700c306cdd6843f17f47b8e605e9fb56a6b0f489
Sikandar Ali <sikandarali94@hotmail.com>
2017-12-24 21:27:34 +1100

    finished sec5 lec63 binding to custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,4 +20,26 @@ export class AppComponent {
   is both the app component and cockpit component require the serverElements
   array. Without serverElements in both components our app will crash.
    */
+
+  /* These methods would only be executed after the Add Server or Add Server
+  Blueprint button has been clicked. So not once the button has been clicked
+  but after button has been clicked.
+  serverData is type of object we expect to be passed to the onServerAdded()
+  method.
+   */
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+        type: 'server',
+        name: serverData.serverName,
+        content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+        type: 'blueprint',
+        name: blueprintData.serverName,
+        content: blueprintData.serverContent
+    });
+  }
 }
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+/* If we are creating custom events we need to import EventEmitter so we can
+use it in our component model below.
+The @Output decorator needs to imported so we can use it in our components as
+shown below.
+ */
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,6 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+  /* To make serverCreated and blueprintCreated custom events we need them to be
+  an EventEmitter type as show below.
+  EventEmitter is a generic type and thus we use <> sign to indicate that
+  EventEmitter is a generic type to Typescript as shown below. Inside we indicate
+  the type of event data we are going to emit. In the end we need to place the
+  parenthesis at the end of EventEmitter to call the constructor of event emitter
+  and create a new event emitter object which is now stored in serverCreated.
+  However, we need to make serverCreated listenable from outside this component.
+  We don't use the @Input() decorator because we are not getting something passed
+  into this component, it's @Output because we are passing something out of the
+  component.
+   */
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
   newServerName = '';
   newServerContent = '';
 
@@ -14,20 +34,19 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
+  /* We want to tell the parent component of cockpit component (the app-component) that a
+  new server or a new blueprint was created.
+   */
   onAddServer() {
-      // this.serverElements.push({
-      //     type: 'server',
-      //     name: this.newServerName,
-      //     content: this.newServerContent
-      // });
+    /* To emit the serverCreated custom event we call the emit() method as shown below.
+    Inside the emit object we pass an object where we have a serverName and serverContent
+    as we defined them in the custom events above.
+     */
+    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
   }
 
   onAddBlueprint() {
-      // this.serverElements.push({
-      //     type: 'blueprint',
-      //     name: this.newServerName,
-      //     content: this.newServerContent
-      // });
+      this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
   }
 
 }
