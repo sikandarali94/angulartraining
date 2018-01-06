ef5b5ca0bb798da997400ce5e6b2927fa8f47f29
Sikandar Ali <sikandarali94@hotmail.com>
2018-01-06 14:25:03 +1100

    finished sec6 lec77 adding navigation with event binding and ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
+  showRecipes = true;
+  @Output() viewState = new EventEmitter<{recipeState: boolean}>();
+  showState (link: string) {
+    if (link === 'recipe') {
+      return this.showRecipes === true ? '#333' : '#777';
+    } else if (link === 'shopping') {
+      return this.showRecipes === true ? '#777' : '#333';
+    }
+  }
+  stateEmit() {
+    this.viewState.emit({recipeState: this.showRecipes});
+  }
 }
