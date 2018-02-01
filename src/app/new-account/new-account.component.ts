a5d6d132ed455476489f57bf47ee8ff34519d63d
Sikandar Ali <sikandarali94@hotmail.com>
2018-02-01 17:13:31 +1100

    finished sec9 lec98 injecting the logging service into components

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,12 +1,33 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+/* We must first import our services before we can inject them into our component for use.
+ */
+import {LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  /* In providers we specify the type of what we want to be able to get provided. So,
+  in this case we specify LoggingService again. With this Angular when analyzing the
+  component recognises that it should be able to give us a LoggingService and it will
+  set itself up to be able to do so. And when Angular actually constructs the component
+  it sees we want to have such an instance of and it will know how to give us such an
+  instance.
+   */
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  /* We can name our dependency whatever we like. In this case we name it loggingService.
+  However we cannot name our type whatever we like. We have to provide the exact name
+  of the Service we want injected into our component. This tells Angular that new-account
+  component depends on the LoggingService to function and thus Angular constructs the
+  component in such a way that it creates an instance of the service for the component.
+  The last thing we need to tell Angular is how to give us such an instance of
+  LoggingService in this component.
+   */
+  constructor(private loggingService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
@@ -15,6 +36,11 @@ export class NewAccountComponent {
     });
     /* We want to create a service that can log data.
      */
-    console.log('A server status changed, new status: ' + accountStatus);
+    /* The reason it is better to use this method of creating an instance of our service
+    rather than doing it by using the new keyword manually is that allows us to stay in
+    the Angular ecosystem where Angular knows how our app works. There are other advantages
+    we will encounter as we use Angular more.
+     */
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
