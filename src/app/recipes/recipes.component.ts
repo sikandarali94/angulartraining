1481cf7306cedfe745027fc7d35bfab007261f25
Sikandar Ali <sikandarali94@hotmail.com>
2018-03-03 23:46:26 +1100

    finished sec10 lec107 using a service for cross-component communication

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -11,9 +11,14 @@ import { RecipeService } from './recipes.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(
+        (recipe: Recipe) => {
+          this.selectedRecipe = recipe;
+        }
+    );
   }
 
 }
