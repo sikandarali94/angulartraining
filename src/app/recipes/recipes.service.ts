7aeeb969a9b0156e652a3270dc951ffc2088b443
Sikandar Ali <sikandarali94@hotmail.com>
2018-03-03 23:17:32 +1100

    finished sec10 lec106 managing recipes in a recipe service

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,3 +1,16 @@
-export class RecipeService {
+import {Recipe} from './recipe.model';
 
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    ];
+
+    getRecipes() {
+        /* Using slice() like we did below makes a copy of the array, which means when we return the recipes
+        array we don't return a reference to the recipes array in our service. This makes sure that any changes
+        to a recipes array does not affect the original recipes array in our service.
+         */
+        return this.recipes.slice();
+    }
 }
