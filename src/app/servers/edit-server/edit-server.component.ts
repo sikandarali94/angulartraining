8a6c163073c7150b3197ba02bd87cd1d0fdf3f2c
Sikandar Ali <sikandarali94@hotmail.com>
2018-10-29 02:38:19 +1100

    finished sec11 lec128 retrieving query parameters and fragments

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -12,9 +12,20 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    /* This is one method below in how we can access query parameter values and fragment values in the current URL. However, this would
+    not update the component that is currently loaded which has the data passed in the URL altered.
+     */
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+    /* To counter the above issue, route object has queryParams and fragment observables that reload the currently loaded component if
+    the data passed to that component from the URL has changed. We don't have to manually unsubscribe to these observables if the component
+    is destroyed as Angular would do it automatically for us.
+     */
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
