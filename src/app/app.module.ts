b7a30572fe4d5bb84882d743000cb6b46551b3a3
Sikandar Ali <sikandarali94@hotmail.com>
2018-10-29 21:58:07 +1100

    finished sec11 lec134 important: redirection path matching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,6 +49,19 @@ const appRoutes: Routes = [
     routes. This is because our routes get parsed top to bottom.
      */
     { path: '**', redirectTo: '/not-found' }
+    /* By default, Angular matches paths by prefix. That means, that the following route will match both '/recipes'  and just '/':
+    { path: '', redirectTo: '/somewhere-else' }
+
+    Actually, Angular will give you an error here, because that's a common gotcha: This route will now ALWAYS redirect you! Why?
+
+    Since the default matching strategy is "prefix" , Angular checks if the path you entered in the URL does start with the path specified
+    in the route. Of course every path starts with ''  (Important: That's no whitespace, it's simply "nothing").
+
+    To fix this behavior, you need to change the matching strategy to "full":
+    { path: '', redirectTo: '/somewhere-else', pathMatch: 'full' }
+
+    Now, you only get redirected, if the full path is ''  (so only if you got NO other content in your path in this example).
+     */
 ];
 
 @NgModule({
