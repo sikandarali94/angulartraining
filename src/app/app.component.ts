0559888bef663808e6440d9a462f21a0a0a05e0a
Sikandar Ali <sikandarali94@hotmail.com>
2018-11-14 11:48:55 +1100

    finished sec15 lec177 td: submitting and using the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+/* We must import NgForm from '@angular/forms' before we can use it in our TypeScript file.
+ */
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  /* When we receive the reference to the form, it is of type: ElementRef. However, when we pass a reference to the form that has accessed
+  ngForm, it becomes of type: NgForm. This NgForm object has the value object which includes all the values of the controls we have
+  defined in our form.
+   */
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
