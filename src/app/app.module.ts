2ea4a72b4610083e79d16222f5fac90750d8e1a5
Sikandar Ali <sikandarali94@hotmail.com>
2018-11-14 11:00:18 +1100

    finished sec15 lec176 td: creating the form and registering the controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+/* To handle forms with Angular, we first need to make sure to import FormsModule from '@angular/forms'. By default, in our CLI, FormsModule
+is included as part of the imports already for us.
+ */
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -11,6 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    /* We need to include FormsModule in our imports to use it in our app.
+     */
     FormsModule,
     HttpModule
   ],
