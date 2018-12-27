771d6dd2e65ef65a4b0f620dc8cd211c044d3233
Sikandar Ali <sikandarali94@hotmail.com>
2018-12-27 17:41:02 +1100

    finished sec20 lec258 more about jwt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 /* Most of the authentication happens on the backend but it is important to understand how to handle authentication on the frontend.
  */
+/* JSON Web Token (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting information
+between parties as a JSON object. This information can be verified and trusted because it is digitally signed. JWTs can be signed using a
+secret (with the HMAC algorithm) or a public/private key pair using RSA or ECDSA.
+Authorization: This is the most common scenario for using JWT. Once the user is logged in, each subsequent request will include the JWT,
+allowing the user to access routes, services, and resources that are permitted with that token. Single Sign On is a feature that widely uses
+JWT nowadays, because of its small overhead and its ability to be easily used across different domains.
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
