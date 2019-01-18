863ecc7e99c9dab9b268d9e1942812fa84b2808b
Sikandar Ali <sikandarali94@hotmail.com>
2019-01-18 16:03:07 +1100

    finished sec20 lec263 signin users in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.compo
 import {RecipeSelectComponent} from './recipes/recipe-select/recipe-select.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 import {SignupComponent} from './auth/signup/signup.component';
+import {SigninComponent} from './auth/signin/signin.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -16,7 +17,8 @@ const routes: Routes = [
         { path: ':id/edit', component: RecipeEditComponent }
     ]},
     { path: 'shopping-list', component: ShoppingListComponent },
-    { path: 'signup', component: SignupComponent }
+    { path: 'signup', component: SignupComponent },
+    { path: 'signin', component: SigninComponent }
 ];
 
 @NgModule({
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,12 +6,28 @@ export class AuthService {
   /* This method will be used for signing up a user.
    */
   signupUser(email: string, password: string) {
-    /* createUserWithEmailAndPassword is a firebase method that returns a promise. That is the reason we are using the .catch() method to
+    /* createUserWithEmailAndPassword is a firebase method that returns a promise. That is the reason we are using the .then() method to
     grab any errors that we want to print to the console.
      */
     firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(
-        () => console.log('yo')
+      .catch(
+        (error) => console.log(error)
       );
+  }
+
+  signinUser(email: string, password: string) {
+    /* The firebase package has a method called signInWithEmailAndPassword() which authenticates with Firebase whether the user is valid.
+     */
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        /* The response we get if authentication is successful is a JSON Web Token. We can see this token in the developer tools in Chrome: go
+        to Application->IndexedDB and there we will find the JSON Web Token.
+         */
+        response => console.log(response)
+      )
+      .catch(
+        error => console.log(error)
+      );
+
   }
 }
