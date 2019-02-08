fa61f0325a9ec12dbc6196958975ddf473c7dced
Sikandar Ali <sikandarali94@hotmail.com>
2019-02-08 19:33:50 +1100

    finished sec21 lec281 loading components via selectors vs routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+/* For routing it is not important that we declare a component in the same file as the routes live. It's just important that we declare them
+anywhere in our application before we get a chance of visiting the route. However, for the selector of a component we have to declare it in
+the module where we plan to use the selector OR we have to import another module which exports that thing.
+ */
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
