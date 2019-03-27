f1ca125c3f40630d86b00265ad8f55f137cfff43
Sikandar Ali <sikandarali94@hotmail.com>
2019-03-27 12:40:50 +1100

    finished sec24 lec335 getting state access in http interceptor

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,14 +1,21 @@
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
+import {Store} from '@ngrx/store';
 
-import { AuthService } from './auth.service';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from './store/auth.reducers';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.authService.isAuthenticated();
+    /* Here we are grabbing the authenticated value from the state and returning it.
+     */
+    return this.store.select('auth').map((authState: fromAuth.State) => {
+      return authState.authenticated;
+    });
   }
 }
--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,18 +1,32 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
+import {Store} from '@ngrx/store';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
 
-import { AuthService } from '../auth/auth.service';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted!', req);
     // const copiedReq = req.clone({headers: req.headers.set('', '')});
-    const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
-    return next.handle(copiedReq);
+    /* We have a little issue here. In the past we could our token synchronously but now we get it asynchronously, because selecting a slice
+    of our state yields us an observable (which is an asynchronous object). To fix this issue, we realise that the intercept method requires
+    that we output an observable and selecting a slice of our state yields an observable. Thus we return the selecting slice state
+    observable, as shown below. We use switchMap because otherwise the observable returned by next.handle(copiedReq) will be wrapped by the
+    observable returned by this.store.select('auth'). By using the switchMap it won't return an observable wrapped by another observable but
+    instead use the returned value (next.handle(copiedReq)) which is an observable.
+     */
+    return this.store.select('auth')
+      .switchMap((authState: fromAuth.State) => {
+        const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
+        return next.handle(copiedReq);
+      });
     // return null;
   }
 }
