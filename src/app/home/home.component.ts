f640406996269cd4db620eb6f7b573c17c185660
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-23 20:10:17 +1000

    finished sec13 lec171 building a custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -17,8 +17,25 @@ export class HomeComponent implements OnInit, OnDestroy {
     /* A simple method to create an observable is using the interval method, as shown below. it is very similar to setInterval() method,
     except that it emits a new value (starting from 0 and incrementing by 1) every certain amount of time (we provide that interval as
     milliseconds, as shown below). */
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    /* The observer only cares about new data coming in, about an error or about the observable being completed. In other words, the
+    observer is the listener. We are creating an observable similar to the one provided by the interval() method that we commented out
+    above. */
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        /* We use the .next() method to emit a new value. We use observer.error() to emit an error. We use observer.complete() to signal
+        that the observable is complete. */
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
     });
   }
 
