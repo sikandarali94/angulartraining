423edc5278a9d16f176574a0b3fa7abc709e0dd4
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-28 19:13:24 +1000

    finished sec14 lec178 improving the reactive service with observables (subjects)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-
-import { UserService } from '../user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  private activatedSub: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    /* Just like with typical observables, we should unsubscribe from our Subject observables as well when we don't need them. */
-    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
-      this.userActivated = didActivate;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.activatedSub.unsubscribe();
-  }
-}
+export class AppComponent {}
