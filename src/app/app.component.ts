e27d4d3035c507d1d38d2b19a21d1dcdede6b582
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-26 15:49:40 +1100

    finished sec18 lec251 how does angular interact with backends?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,7 @@
+/* We don't actually connect Angular to a database directly, meaning we don't enter our database credentials into our Angular app or
+anything like that. This is because everyone can inspect the Angular code. We instead send HTTP requests from Angular to a server and get
+a HTTP response from the server back. To interact with the server, we do it through its API. The server is the one that then interacts with
+the DB if interacting with the DB is required. */
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
