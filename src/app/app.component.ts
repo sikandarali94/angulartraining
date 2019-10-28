5eb283f1a3f381eb662e13ae5afdf8138023ba79
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-28 19:16:21 +1100

    finished sec18 lec254 sending a post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ anything like that. This is because everyone can inspect the Angular code. We in
 a HTTP response from the server back. To interact with the server, we do it through its API. The server is the one that then interacts with
 the DB if interacting with the DB is required. */
 import { Component, OnInit } from '@angular/core';
+/* We import the HttpClient from '@angular/common/http', as shown below. */
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -13,12 +14,20 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
+  /* We inject the HttpClient, as shown below. */
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
+    /* The HttpClient has RESTful verbs we can use to send HTTP requests as methods e.g. post(), delete(), get(), patch(), put(). The post()
+    method takes the request URL as the first argument, the URL body as the second argument (Angular automatically converts the JS object to
+    JSON). Angular uses observable with these HTTP request methods and that is why we must subscribe to them in order for the request to be
+    sent successfully. We don't have to manually unsubscribe, because Angular does it automatically anyway with these methods. */
+    this.http.post('https://ng-recipe-book-82253.firebaseio.com/posts.json', postData).subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+/* We have to import the HttpClientModule to send HTTP requests with Angular, as shown below.*/
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
+  /* Make sure to add HttpClientModule to the imports array, as shown below. */
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
