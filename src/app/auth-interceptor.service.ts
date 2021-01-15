83e55b969648253cf5449100eea30c3b4a2bfa0b
Sikandar Ali <sikandarali94@hotmail.com>
2021-01-16 08:41:54 +1100

    finished sec18 lec272 manipulating request objects

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -6,9 +6,15 @@ export class AuthInterceptorService implements HttpInterceptor {
   forwarded to subscribe (using next). Therefore, with an interceptor, for example, we can attach custom headers to all outgoing requests
   in order to authenticate a user. The req object has all the information about the request, like the URL and so on. */
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    /* The request object is immutable, so we cannot do something like req.url = 'something else'. Therefore, if we want to modify a
+    request we have to create a new one using the clone method (in the object passed to the clone method, we overwrite all the core things
+    of the request), as shown below. */
+    const modifiedRequest = req.clone({ headers: req.headers.append('Auth', 'xyz') });
     console.log('Request is on its way');
+    console.log(req.url);
     /* We use next to let the request continue after modifying it. Otherwise, the request will not continue and could break our app that
     is reliant on the request. */
-    return next.handle(req);
+    // After creating the modified request, we then forward the modified request, as shown below.
+    return next.handle(modifiedRequest);
   }
 }
